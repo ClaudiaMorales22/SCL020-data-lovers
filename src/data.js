5a528e86030dd912a2e470f2c8eae8fcdf6cab8c
ClaudiaMorales22 <claudia.extreme2204@gmail.com>
2022-05-10 15:39:18 -0400

    actualizacion html yCSS

--- src/data.js
+++ src/data.js
@@ -1,9 +1,12 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+export const getFemaleAthletes = (data) => {
+    const athletes = data.athletes;
+    const femaleathletes = athletes.filter(person => person.gender === 'F');
+  data.forEach(femaleathletes);
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+
+export const femaleteam = (femaleathletes, valor) => {
+  const femaleTeam = femaleathletes.filter(person => (person.team === valor || valor === 'All'));
+  return femaleTeam ;
 };
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/athletes/athletes.js';
+import { femaleteam } from './data.js';
 
-console.log(example, data);
+const athletes = data.athletes;
+const femaleathletes = athletes.filter(person => person.gender === 'F')
+
+document.addEventListener('DOMContentLoaded', () => {
+    const femaleathletesContainer = document.querySelector('#allAthletes');
+    for (let i=0; i<femaleathletes.length; i++){
+        const athleteCard = document.createElement('div')
+        athleteCard.className = 'athlete'
+          athleteCard.innerHTML += `
+              <img src="${('images/Niña.jpeg')}" alt="">
+                  <p>Nombre: ${femaleathletes[i].name}</p>
+                  <p>País: ${femaleathletes[i].team}</p>
+                  <p>Deporte: ${femaleathletes[i].sport}</p>
+                  <p>Medalla: ${femaleathletes[i].medal}</p>
+          `;
+          femaleathletesContainer.appendChild(athleteCard);
+    }   
+  });
+  
